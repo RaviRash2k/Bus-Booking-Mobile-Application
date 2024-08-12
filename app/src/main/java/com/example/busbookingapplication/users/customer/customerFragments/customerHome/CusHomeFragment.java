5430cee2423ff02bd2fec6a4414cd3d905459f16
RaviRash2k <150897047+RaviRash2k@users.noreply.github.com>
2024-08-12 18:25:45 +0530

    create interfaces of customer book and menu

--- app/src/main/java/com/example/busbookingapplication/users/customer/customerFragments/customerHome/CusHomeFragment.java
+++ app/src/main/java/com/example/busbookingapplication/users/customer/customerFragments/customerHome/CusHomeFragment.java
@@ -1,4 +1,4 @@
-package com.example.busbookingapplication.users.customer.customerFragments;
+package com.example.busbookingapplication.users.customer.customerFragments.customerHome;
 
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
