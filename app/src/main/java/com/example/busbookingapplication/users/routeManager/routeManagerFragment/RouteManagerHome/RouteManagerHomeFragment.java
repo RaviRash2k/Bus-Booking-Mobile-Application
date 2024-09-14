4dcffb449c0017bc2179e3418c9cfb2778e81bcf
RaviRash2k <150897047+RaviRash2k@users.noreply.github.com>
2024-09-15 02:32:26 +0530

    function for view daily travelers tickets

--- app/src/main/java/com/example/busbookingapplication/users/routeManager/routeManagerFragment/RouteManagerHome/RouteManagerHomeFragment.java
+++ app/src/main/java/com/example/busbookingapplication/users/routeManager/routeManagerFragment/RouteManagerHome/RouteManagerHomeFragment.java
@@ -12,21 +12,32 @@ import android.widget.TextView;
 
 import com.example.busbookingapplication.R;
 import com.example.busbookingapplication.users.routeManager.routeManagerFragment.RouteManagerHome.BusManage.BusManage;
+import com.example.busbookingapplication.users.routeManager.routeManagerFragment.RouteManagerHome.ViewTicket.ViewTickets;
 
 public class RouteManagerHomeFragment extends Fragment {
 
-    TextView busManage;
+    TextView busManage, viewTicket;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View rootView = inflater.inflate(R.layout.fragment_route_manager_home, container, false);
 
-        //textViews
+        //busRegister
         busManage = rootView.findViewById(R.id.busManage);
         busManage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(getContext(), BusManage.class));
+            }
+        });
+
+        //viewTicket
+        viewTicket = rootView.findViewById(R.id.viewTicket);
+        viewTicket.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getContext(), ViewTickets.class));
             }
         });
 
